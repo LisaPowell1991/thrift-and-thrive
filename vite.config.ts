adf221dba8c12b64fc86870c6c71615f9c7ede23
Lisa Powell <lisapowell46@gmail.com>
2024-02-15 10:00:06 +0200

    package assets on build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import copy from 'vite-plugin-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build:{
     assetsDir: 'src/assets',
   },  
-  plugins: [react()],
+  plugins: [
+    react(),
+    copy({
+      targets: [
+        { src: '/src/assets/images/*', dest: 'dist/assets/images' }
+      ],
+      hook: 'writeBundle' // run the plugin only when writing the bundle
+    })
+  ],
 })
