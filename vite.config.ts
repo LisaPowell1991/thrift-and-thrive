109021f63b08a7081a8ca7cf9c6c1c1451bcaefb
lisapowell1991 <lisapowell46@gmail.com>
2024-02-15 10:37:06 +0000

    chhanges to config files

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import copy from 'vite-plugin-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build:{
     assetsDir: 'src/assets',
   },  
-  plugins: [
-    react(),
-    copy({
-      targets: [
-        { src: '/src/assets/images/*', dest: 'dist/assets/images' }
-      ],
-      hook: 'writeBundle' // run the plugin only when writing the bundle
-    })
-  ],
+  plugins: [react()],
 })
